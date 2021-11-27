aba10efd507186bf2401a281b707e111f856afc5
Kevin <sxzz@sxzz.moe>
2021-11-28 00:00:51 +0800

    fix: vue ref sugar types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,4 @@
 /* eslint-disable import/no-duplicates */
-/// <reference types="vite/client" />
-/// <reference types="vue/ref-macros" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
